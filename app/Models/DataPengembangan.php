92a6ec26a05b76bea09aa86299d2b6d9631ab55c
debbyfrandina <74141415+debbyfrandina@users.noreply.github.com>
2023-01-25 21:45:16 +0700

    up

--- app/Models/DataPengembangan.php
+++ app/Models/DataPengembangan.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class DataPengembangan extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nama_program',
+        'nama_kegiatan',
+        'nama_sub_kegiatan',
+        'nama_sub_sub_kegiatan',
+        'tanggal_pelaksanaan',
+        'tempat_pelaksanaan',
+        'jumlah_peserta',
+        'jumlah_produk'
+    ];
 }
--- database/seeders/DataPengembanganSeeder.php
+++ database/seeders/DataPengembanganSeeder.php
@@ -14,6 +14,17 @@ class DataPengembanganSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('pengembangans')->insert([
+            [
+                'nama_program' => 'Program Pengelolaan Pendidikan',
+                'nama_kegiatan' => 'Pengelolan Pendidikan SMK',
+                'nama_sub_kegiatan' => 'Pengadaan Alat & Peraga Siswa',
+                'nama_sub_sub_kegiatan' => '-Aplikasi prototype media pembelajaran berbasis edugame -Vodcast berbagi',
+                'tanggal_pelaksanaan' => '01/01/23 - 01/07/23',
+                'tempat_pelaksanaan' => 'SMK N Tunas Bangsa Magelang',
+                'jumlah_peserta' => '1000',
+                'jumlah_produk' => '3',
+            ],
+        ]);
     }
 }
