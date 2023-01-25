2747d2fcb2f7748a4c8b1456d783c9322c893a9f
debbyfrandina <74141415+debbyfrandina@users.noreply.github.com>
2023-01-25 17:45:11 +0700

    up

--- resources/views/form_pengembangan.blade.php
+++ resources/views/form_pengembangan.blade.php
@@ -93,12 +93,12 @@
 
         <div class="card-body">
           <h5 class="card-title mb-0">Tanggal Pelaksanaan</h5><br>
-          <input type="date-local">
+          <input type="datetime-local">
         </div>
 
         <div class="card-body">
           <h5 class="card-title mb-0">Tanggal Akhir Pelaksanaan</h5><br>
-          <input type="date-local">
+          <input type="date">
         </div>
 
         <div class="card-body">
